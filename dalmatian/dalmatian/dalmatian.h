ed13f93a2ba70b602d4a5603139283dfecfc5385
KO-YAMAKO <ko.foot1103@gmail.com>
2018-11-02 15:41:22 +0900

    new library project

---
+++ dalmatian/dalmatian/dalmatian.h
@@ -0,0 +1,19 @@
+//
+//  dalmatian.h
+//  dalmatian
+//
+//  Created by 山浦功 on 2018/11/02.
+//  Copyright © 2018 com.stairs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for dalmatian.
+FOUNDATION_EXPORT double dalmatianVersionNumber;
+
+//! Project version string for dalmatian.
+FOUNDATION_EXPORT const unsigned char dalmatianVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <dalmatian/PublicHeader.h>
+
+
